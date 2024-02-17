aec8dbd1bc4203972911001fea785f01d66e937f
ray lee <2lee.raymond@gmail.com>
2024-02-17 13:32:35 -0600

    Refine drop rate calculations

--- analysis.py
+++ analysis.py
@@ -27,30 +27,42 @@ def calculate_drop_rate(sent_dfs, rcvd_dfs):
     drop_rate_data = []
 
     for sent_df in sent_dfs:
-        sender = sent_df['sender'].iloc[0]
-        total_sent = len(sent_df)
-        if any(rcvd_df['recipient'].iloc[0] == sender for rcvd_df in rcvd_dfs):
-            rcvd_df = next(rcvd_df for rcvd_df in rcvd_dfs if rcvd_df['recipient'].iloc[0] == sender)
-            total_rcvd = len(rcvd_df)
-        else:
-            total_rcvd = 0
-        dropped = total_sent - total_rcvd
-        drop_rate = dropped / total_sent if total_sent > 0 else 0
+        sender = sent_df["sender"].iloc[0]
+        sent_uuids = set(sent_df["uuid"])
 
-        drop_rate_data.append({
-            'sender': sender,
-            'total_sent': total_sent,
-            'total_rcvd': total_rcvd,
-            'dropped': dropped,
-            'drop_rate': drop_rate
-        })
+        total_sent = len(sent_uuids)
+        # total_received = 0
 
-    return pd.DataFrame(drop_rate_data)
+        for rcvd_df in rcvd_dfs:
+            recipient = rcvd_df["recipient"].iloc[0]
+            if recipient != sender:
+                received_uuids = set(rcvd_df["uuid"])
+                total_received = len(sent_uuids.intersection(received_uuids))
+
+                # print(
+                #     f"Drop rate for {sender}->{recipient}: {1 - total_received / total_sent}"
+                # )
+                total_dropped = total_sent - total_received
+                drop_rate = total_dropped / total_sent if total_sent > 0 else 0
+                drop_rate_data.append(
+                    {
+                        "sender": sender,
+                        "recipient": recipient,
+                        "total_sent": total_sent,
+                        "total_received": total_received,
+                        "total_dropped": total_dropped,
+                        "drop_rate": drop_rate,
+                    }
+                )
+
+    return pd.DataFrame(drop_rate_data).sort_values(["sender", "recipient"])
 
 
 def analysis_orchestrator(logs_path, plot_graphs):
     sent_csv_files, rcvd_csv_files = get_files(logs_path)
-    sent_dfs_arr, rcvd_dfs_arr = generate_dataframes(sent_csv_files), generate_dataframes(rcvd_csv_files)
+    sent_dfs_arr, rcvd_dfs_arr = generate_dataframes(
+        sent_csv_files
+    ), generate_dataframes(rcvd_csv_files)
 
     sent_dfs = pd.concat(sent_dfs_arr)
 
@@ -74,23 +86,30 @@ def analysis_orchestrator(logs_path, plot_graphs):
         n = latency.count()
 
         # Append a dictionary of statistics to the list
-        aggregate_stats_data.append({
-            'recipient': df_sorted['recipient'].iloc[0],
-            'mean': mean_latency,
-            'median': median_latency,
-            'max': max_latency,
-            'min': min_latency,
-            'std': std_latency,
-            'q1': q1_latency,
-            'q3': q3_latency,
-            'n': n,
-            'p99': p99_latency,
-        })
+        aggregate_stats_data.append(
+            {
+                "recipient": df_sorted["recipient"].iloc[0],
+                "mean": mean_latency,
+                "median": median_latency,
+                "max": max_latency,
+                "min": min_latency,
+                "std": std_latency,
+                "q1": q1_latency,
+                "q3": q3_latency,
+                "n": n,
+                "p99": p99_latency,
+            }
+        )
 
         if plot_graphs:
             # Plot latency
             plt.figure(idx)
-            plt.plot(df_sorted["timestamp_x"].values, latency.values, label=f"Sender: {df_sorted['sender'].iloc[0]}", marker="o")
+            plt.plot(
+                df_sorted["timestamp_x"].values,
+                latency.values,
+                label=f"Sender: {df_sorted['sender'].iloc[0]}",
+                marker="o",
+            )
             plt.xlabel("Timestamp X")
             plt.ylabel("Latency (Timestamp Y - Timestamp X)")
             plt.title(f'Latency by Timestamp for Sender: {df_sorted["sender"].iloc[0]}')
@@ -105,7 +124,6 @@ def analysis_orchestrator(logs_path, plot_graphs):
 
     # Print aggregate statistics without the index column
     print(aggregate_stats.to_string(index=False))
-    print(type(sent_dfs_arr[0]))  # This should output <class 'pandas.core.frame.DataFrame'>
     drop_rate_df = calculate_drop_rate(sent_dfs_arr, rcvd_dfs_arr)
     print(drop_rate_df.to_string(index=False))
 
@@ -113,7 +131,9 @@ def analysis_orchestrator(logs_path, plot_graphs):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run offline analysis on logs data")
     parser.add_argument("--logs_path", type=str, help="Logs data path")
-    parser.add_argument("--plot", action="store_true", help="Plot the latency graphs (default: False)")
+    parser.add_argument(
+        "--plot", action="store_true", help="Plot the latency graphs (default: False)"
+    )
     args = parser.parse_args()
 
     if args.logs_path:
